631123b56367fa8faaff5858d2c0e5703660fed3
ironicsushi <yuli5565@colorado.edu>
2017-08-18 10:00:54 -0500

    Function Exercise
    * created a function to determine even and odd numbers
    * created a tip calculator

--- js/functions.js
+++ js/functions.js
@@ -10,16 +10,10 @@
  */
 
 function sayHello(name) {
-  "Hello, " + name + "!";
+  return "Hello, " + name + "!";
 }
 
-console.log(sayHello("codeup"));
-
-function increment(x) {
-    x + 1;
-}
-
-var four = increment(3);
+console.log(sayHello("Codeup"));
 
 /**
  * TODO:
@@ -30,6 +24,11 @@ var four = increment(3);
  * variable to the 'sayHello' function. You should see the same output in the
  * console.
  */
+
+var helloMessage = sayHello("Joyce");
+console.log(helloMessage);
+var myName = "Joyce"
+console.log(sayHello(myName));
 
 // Don't modify the following line, it generates a random number between 1 and
 // 100 and stores it in a variable named random
@@ -49,6 +48,20 @@ var random = Math.floor((Math.random() * 100) + 1);
  * console.log *outside of the function* to check your work
  */
 
+function isOdd(number) {
+    var evenOrOdd = (number % 2 === 0) ? number + " is even!" : number + " is odd!";
+    return evenOrOdd;
+}
+
+console.log(isOdd(31));
+console.log(isOdd(43));
+console.log(isOdd(356));
+console.log(isOdd(324234));
+console.log(isOdd(80));
+console.log(isOdd(9090));
+console.log(isOdd(357));
+console.log(isOdd(47));
+
 /**
  * TODO: Create a function named 'calculateTip' to calculate a tip on a bill at a
  * restaurant
@@ -59,6 +72,20 @@ var random = Math.floor((Math.random() * 100) + 1);
  * Example
  *  > calculateTip(0.20, 20) // returns 4
  */
+
+
+        var userPercentage = prompt("Please enter your desired tip percentage.");
+        var userTotal = prompt("Please enter your total bill.");
+
+        function calculateTip(percentage, total) {
+            var percentToTip = (percentage/100) * total;
+            console.log("Your amount to tip is: " + percentToTip);
+            return percentToTip;
+        }
+
+
+        var userTip = calculateTip(userPercentage, userTotal);
+        alert("Your tip should be " + userTip + " dollars.");
 
 /**
  * TODO: use prompt and alert in combination with your calculateTip function to
